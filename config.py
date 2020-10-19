0c43b6f66e19ffa19a66269d1b34b621d079c4f4
AdamSawyer4815162342 <73134174+AdamSawyer4815162342@users.noreply.github.com>
2020-10-19 21:20:06 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+#Токен бота
+token = "1338167390:AAG3HnysW_zYraYneVyCe-ZRi8tYZgqW9aI"
+#ID админов
+admins_id = [435249326]
+#Бонус
+welcome_bonus = 0.0
+#Название бота
+bot_name = "@wooowmoney_bot"
+#Номер киви
+qiwi_num = "79373282652"
+#Апи
+qiwi_api = "48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iP69vngReyX9sn2jRKm3iPpe7aYYWLP6PDdX24R7rxLZQrPyubZfenp2e3q6YzWeKLSEyjNHM7Vm8y2HMrCJZLk2na8cDTeTqJeqp8636Li"
+apibtc = ""
+#Помощь dice
+dice_help = "Помощь"
+#Игровая коммисиия
+games_comission = 5
+#Комиссия на вывод
+withdraw_comission = 2
+#Ссылка на чат
+chat_link = ""
+#Ссылка на канал с отзывами
+votes_link = ""
+#Правила бота
+rules_bot = "Правила бота"
+#Минимальная сумма ставки
+min_sum_bet = 10
+#Максимальная сумма ставки
+max_sum_bet = 15000
+#ID чатов для отправки уведомлений о играх
+chats_ids = []
